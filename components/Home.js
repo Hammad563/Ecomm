889dd5b54ffbbe2404f47e36852d104736ce5317
Hammad563 <munirhammad786@gmail.com>
2021-09-27 13:55:51 -0400

    first commit

---
+++ components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Product from './Product';
+import Title from './Title';
+import { ProductConsumer } from '../context';
+import react from 'react';
+
+export default class Details extends Component{
+    render() {
+        return (
+           <react>
+               <div>
+                   <h3>hello from home</h3>
+               </div>
+           </react>
+        )
+    }
+}
